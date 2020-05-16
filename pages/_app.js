e46a62bd5784042ecdb9003f9e4ee0812ef71544
Michael Dinh <michaeldinh1994@gmail.com>
2020-05-16 16:57:53 -0700

    portfolio updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/styles.scss';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { useState } from 'react';
+import { CssBaseline } from '@material-ui/core';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [light, setLight] = useState(true);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: light ? 'light' : 'dark',
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} setLight={setLight} light={light} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,100 @@
 import Head from 'next/head';
 import { FullPage, Slide } from 'react-full-page';
 import Typed from 'react-typed';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@material-ui/core';
 
-const theme = {
-  palette: {
-    type: 'dark',
-  },
-};
+import EmojiObjectsOutlinedIcon from '@material-ui/icons/EmojiObjectsOutlined';
 
 const pageKey = ['Home', 'Projects', 'Connect'];
 
-const Controls = (props) => {
-  const titles = [];
-  for (let i = 0; i < props.slidesCount; i++) {
-    const status = i === props.getCurrentSlideIndex() ? 'active' : 'inactive';
-    titles.push(
-      <div
-        className={`title title-${status}`}
-        onClick={() => {
-          props.scrollToSlide(i);
-        }}
-      >
-        {pageKey[i]}
-      </div>
-    );
-  }
-  return <div className="control">{titles}</div>;
-};
+const items = [
+  {
+    name: 'Nautilus',
+    description: `Reaching Top 10 on HN and and featured on Google's Kubernetes podcast, Nautilus is a Docker Compose file charting visualizer available for Mac, Windows, and Linux.`,
+    img: '',
+    url: 'https://nautilusdev.com/',
+  },
+];
 
-export default function Home() {
+function Item(props) {
+  console.log(props);
   return (
-    <ThemeProvider theme={theme}>
-      <div className="container">
-        <Head>
-          <title>Michael Dinh</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div className="item">
+      <a href={props.item.url}>
+        <h3>{props.item.name}</h3>
+      </a>
+      <p>{props.item.description}</p>
+    </div>
+  );
+}
 
-        <FullPage controls={Controls}>
-          <Slide className="home">
-            <Typed
-              strings={["Hi, I'm Michael"]}
-              className="intro"
-              typeSpeed={60}
-              showCursor={false}
-            />
-            <h3>
-              Hi, I'm Michael Dinh. A full stack engineer always excited to talk
-              about the latest technologies, music and dad shoes.
-            </h3>
-          </Slide>
-          <Slide className="projects">
-            <Typed
-              strings={['Projects']}
-              className="intro"
-              typeSpeed={60}
-              showCursor={false}
-            />
-          </Slide>
-          <Slide className="connect">
-            <Typed
-              strings={["Let's Connect"]}
-              className="intro"
-              typeSpeed={60}
-              showCursor={false}
-            />
-          </Slide>
-        </FullPage>
-      </div>
-    </ThemeProvider>
+export default function Home({ setLight, light }) {
+  const Controls = (props) => {
+    const titles = [];
+    for (let i = 0; i < props.slidesCount; i++) {
+      const status = i === props.getCurrentSlideIndex() ? 'active' : 'inactive';
+      titles.push(
+        <div
+          className={`title title-${status}`}
+          onClick={() => {
+            props.scrollToSlide(i);
+            setSlide(i);
+          }}
+        >
+          {pageKey[i]}
+        </div>
+      );
+    }
+    return <div className="control">{titles}</div>;
+  };
+  return (
+    <div className="container">
+      <Head>
+        <title>Michael Dinh</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <EmojiObjectsOutlinedIcon
+        className="themeToggle"
+        onClick={() => {
+          setLight(!light);
+        }}
+      />
+      <FullPage controls={Controls}>
+        <Slide className="homeSlide">
+          <Typed
+            strings={["Hi, I'm Michael"]}
+            className="intro"
+            typeSpeed={60}
+            showCursor={false}
+          />
+          <h3>
+            A full stack engineer always excited to talk about the latest
+            technologies, music and dad shoes.
+          </h3>
+        </Slide>
+        <Slide className="projectsSlide">
+          <Typed
+            strings={['Projects']}
+            className="projects"
+            typeSpeed={60}
+            showCursor={false}
+          />
+          <Carousel className="carousel">
+            {items.map((item) => (
+              <Item item={item} />
+            ))}
+          </Carousel>
+        </Slide>
+        <Slide className="connectSlide">
+          <Typed
+            strings={["Let's Connect"]}
+            className="intro"
+            typeSpeed={60}
+            showCursor={false}
+          />
+        </Slide>
+      </FullPage>
+    </div>
   );
 }
